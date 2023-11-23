68e2a536c8369a340598aa6aac63a6c9d52695c8
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-11-23 14:40:11 -0300

    adiciona funçoes e imprime valor original de joao e maria no console

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import {deJoaoParaMaria,deMariaParaJoao,getMaria,getJoao,setMaria,setJoao} from "./src/joaoEMaria.js";
+
+let JoaoValor = getJoao();
+let MariaValor = getMaria();
+
+console.log("Joao tem ", JoaoValor, "maçãs e Maria tem ", MariaValor ,"maças.")
+
+
