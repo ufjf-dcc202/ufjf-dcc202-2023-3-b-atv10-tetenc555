03549bbc0c9b6993cd520223b73cbbbc04d39e63
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-11-23 14:44:12 -0300

    configura codigo para joao dar macas para maria e imprimir nova quantidade

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import {deJoaoParaMaria,deMariaParaJoao,getMaria,getJoao,setMaria,setJoao} from "./src/joaoEMaria.js";
+import {deJoaoParaMaria,deMariaParaJoao,getMaria,getJoao,setMaria,setJoao} from "src/joaoEMaria.js";
 
 let JoaoValor = getJoao();
 let MariaValor = getMaria();
 
-console.log("Joao tem ", JoaoValor, "maçãs e Maria tem ", MariaValor ,"maças.")
+console.log("Joao tem ", JoaoValor, "maçãs e Maria tem ", MariaValor ,"maçãs.");
 
+deJoaoParaMaria();
+JoaoValor = getJoao();
+MariaValor = getMaria();
 
+console.log("João deu todas suas maçãs para maria, agora Maria tem ", MariaValor, "maçãs.");
