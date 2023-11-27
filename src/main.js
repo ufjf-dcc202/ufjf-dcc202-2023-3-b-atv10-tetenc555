53c9be92a51ee3353d833815f365218ec55b5ff4
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-11-27 19:50:25 -0300

    retira interação com span (atividade errada)

--- src/main.js
+++ src/main.js
@@ -1,20 +1,11 @@
 import {deJoaoParaMaria,deMariaParaJoao,getMaria,getJoao,setMaria,setJoao} from "/src/joaoEMaria.js";
-//cria vetor span
-const spans = document.querySelectorAll("span");
 
 //define maria e joao
 let maria= getMaria();
 let joao= getJoao();
 
 //imprime valor de Maria e Joao no Console
-console.log("Maria possui ", maria, " maçãs e João possui ", joao, " maçãs.")
-//iguala spans a valor de joao e maria
-spans[0].textContent=joao;
-spans[1].textContent=maria;
-
-//define botao para chamar a funcao de doacao
-const botao =document.querySelector("button")
-botao.addEventListener("click",fazDoacao);
+console.log("Maria possui", maria, "maçãs e João possui", joao, "maçãs.")
 
 //funcao doacao
 function fazDoacao() {
@@ -22,7 +13,7 @@ function fazDoacao() {
     setJoao(0);
     maria=getMaria();
     joao=getJoao();
-    spans[0].textContent=joao;
-    spans[1].textContent=maria;
-    console.log("João deu todas suas maçãs para Maria e agora possui ",joao," maçãs. Por sua vez, Maria possui ",maria," maçãs.")
+    console.log("João deu todas suas maçãs para Maria e agora possui",joao,"maçãs. Por sua vez, Maria possui",maria,"maçãs.")
 }
+
+fazDoacao();
