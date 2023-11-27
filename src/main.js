8dd9a712eff6c3b68f9d5564ee392863617d0e18
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-11-27 19:34:17 -0300

    adiciona div com interação em que as maçãs são trocadas pelo texto e pelo console log

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
-import {deJoaoParaMaria,deMariaParaJoao,getMaria,getJoao,setMaria,setJoao} from "src/joaoEMaria.js";
+import {deJoaoParaMaria,deMariaParaJoao,getMaria,getJoao,setMaria,setJoao} from "/src/joaoEMaria.js";
+//cria vetor span
+const spans = document.querySelectorAll("span");
 
-let JoaoValor = getJoao();
-let MariaValor = getMaria();
+//define maria e joao
+let maria= getMaria();
+let joao= getJoao();
 
-console.log("Joao tem ", JoaoValor, "maçãs e Maria tem ", MariaValor ,"maçãs.");
+//imprime valor de Maria e Joao no Console
+console.log("Maria possui ", maria, " maçãs e João possui ", joao, " maçãs.")
+//iguala spans a valor de joao e maria
+spans[0].textContent=joao;
+spans[1].textContent=maria;
 
-deJoaoParaMaria();
-JoaoValor = getJoao();
-MariaValor = getMaria();
+//define botao para chamar a funcao de doacao
+const botao =document.querySelector("button")
+botao.addEventListener("click",fazDoacao);
 
-console.log("João deu todas suas maçãs para maria, agora Maria tem ", MariaValor, "maçãs.");
+//funcao doacao
+function fazDoacao() {
+    setMaria(maria+joao);
+    setJoao(0);
+    maria=getMaria();
+    joao=getJoao();
+    spans[0].textContent=joao;
+    spans[1].textContent=maria;
+    console.log("João deu todas suas maçãs para Maria e agora possui ",joao," maçãs. Por sua vez, Maria possui ",maria," maçãs.")
+}
